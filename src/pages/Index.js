7f64f3dc6833938d2fc2d24502e47686e8b14c4f
Joe Brucker <joe.r.brucker@gmail.com>
2021-01-04 13:36:58 -0500

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -12,10 +12,6 @@ const Index = () => (
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
-          </p>
         </div>
       </header>
       <p> Welcome to 3W, which is built for both {' '}
